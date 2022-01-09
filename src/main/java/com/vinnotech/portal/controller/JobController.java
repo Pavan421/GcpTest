bbd6aa1d2ee3f30be8e38e60ec059a0d7673fd5a
Pavan421 <pavankumarnarnapati@gmail.com>
2022-01-09 13:47:41 +0530

    job controller post

--- src/main/java/com/vinnotech/portal/controller/JobController.java
+++ src/main/java/com/vinnotech/portal/controller/JobController.java
@@ -11,7 +11,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,7 +37,7 @@ public class JobController {
 	 * @return
 	 */
 	@PreAuthorize(HRPortalConstants.ROLE_ADMIN_HR_RECRUITER_ONLY)
-	@PutMapping("/create")
+	@PostMapping("/create")
 	public ResponseEntity<String> createJob(@RequestBody Job job) {
 		String methodName = "createJob";
 		LOGGER.info(CLASSNAME + ": Entering into the " + methodName + " method");
